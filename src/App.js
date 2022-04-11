5682dbfecffbfead7a84a5566513879fad76c278
mrazadtamal <mrazadt@gmail.com>
2022-04-12 00:54:20 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <p>azad</p>
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 }
 
