4ead422cf24c9f9b5aa711d868ba21ad1a371bbf
mrazadtamal <mrazadt@gmail.com>
2022-04-14 14:37:38 +0600

    .swiper {

--- src/Pages/ProductDescription/ProductDescription.jsx
+++ src/Pages/ProductDescription/ProductDescription.jsx
@@ -14,6 +14,15 @@ const ProductDescription = () => {
         <div className="col-md-6">
           <div className=" m-2 p-3">
             <h1> Product Name : {product?.name}</h1>
+            <h1> Product Price : {product?.price}</h1>
+            <img className=" product-Img m-3 " src={product?.image} alt="" />
+            <p>{product?.description}</p>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className=" m-2 p-3">
+            <h1> Product Name : {product?.name}</h1>
             <img className=" product-Img m-3 " src={product?.image} alt="" />
             <p>{product?.description}</p>
           </div>
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,95 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "./carousel.css";
+
+// import Swiper core and required modules
+import SwiperCore, { EffectCoverflow, Pagination, Autoplay } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Pagination, Autoplay]);
+
+export default function Carousel() {
+  return (
+    <div>
+      <Swiper
+        effect={"coverflow"}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        grabCursor={true}
+        loop={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176636/travel/pexels-petr-ganaj-4129949_1_mtpzf2.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176196/travel/pexels-rhiannon-stone-572740_1_t37bvr.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176636/travel/pexels-robin-rodriguez-4652070_1_k4gpyt.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643177037/travel/pexels-simon-berger-698275_1_sac0pv.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176639/travel/pexels-iva-prime-2028177_1_rzwh5c.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176638/travel/pexels-tural-sadigli-1212060_1_k0vxcv.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176634/travel/pexels-antonio-filigno-2976684_1_tajjvq.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176636/travel/pexels-chris-janda-62600_1_h4eows.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/abidazad/image/upload/v1643176636/travel/pexels-donatello-trisolino-10415591_rl4dqp.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
